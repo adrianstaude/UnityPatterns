e26ee23cdeb1cf37af4502000ab72319b2f4562e
adrianstaude <as@adrianstaude.de>
2019-07-06 10:43:58 +0200

    Fully implemented Prototype Example

---
+++ UnityPatterns/Assets/Creational/Prototype/Scripts/Client.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+
+    public Enemy Drone;
+    public Enemy Tank;
+    public Spawner spawner;
+
+    private Enemy spawnedEnemy;
+    private int droneCounter;
+    private int tankCounter;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown("t"))
+        {
+            spawnedEnemy = spawner.SpawnEnemy(Tank);
+            spawnedEnemy.name = "Tank_Clone" + ++tankCounter;
+            spawnedEnemy.transform.Translate(Vector3.forward * tankCounter);
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            spawnedEnemy = spawner.SpawnEnemy(Drone);
+            spawnedEnemy.name = "Drone_Clone" + ++droneCounter;
+            spawnedEnemy.transform.Translate(Vector3.forward * droneCounter);
+        }
+    }
+}
--- UnityPatterns/Assets/Creational/Prototype/Scripts/Spawner.cs
+++ UnityPatterns/Assets/Creational/Prototype/Scripts/Spawner.cs
@@ -6,17 +6,6 @@ public class Spawner : MonoBehaviour
 {
     public ICopyable copy;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown("t"))
-        {
-        }
-        if(Input.GetKeyDown("d"))
-        {
-            //sSpawnEnemy(Drone);
-        }
-    }
 
     public Enemy SpawnEnemy(Enemy prototype)
     {
